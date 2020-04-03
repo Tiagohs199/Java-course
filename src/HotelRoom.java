c5906b2a245af1ba730ba4b7807871fcc2d535e4
Tiago <tiagohs.199@gmail.com>
2020-04-03 00:26:36 -0300

    tratativa no tamanho dos quartos

--- src/HotelRoom.java
+++ src/HotelRoom.java
@@ -8,15 +8,15 @@ public class HotelRoom {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Room[] vect = new Room[10];
+		
 		
 		System.out.print("How many rooms will be rented? ");
 		int n = sc.nextInt();
 		
+		Room[] vect = new Room[n+1];
 		
 		
-		
-		for(int i=0;n>i;i++) {
+		for(int i=0;i<vect.length;i++) {
 			sc.nextLine();
 			System.out.print("Name: ");
 			String name = sc.nextLine();
@@ -25,9 +25,12 @@ public class HotelRoom {
 			String email = sc.nextLine();
 			System.out.print("Room");
 			int number = sc.nextInt();
-			
+			if(number >n) {
+				System.out.println("Room number unavailable");
+				i=-1;
+			}else {
 			vect[number] = new Room(name,email);
-				
+			}
 		}
 		
 		System.out.println("Rooms occupied");
